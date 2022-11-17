698d22c3ecf01a7cd8bce25c9a97f981df3c039c
lsantos <lsantos@nwsoft.com.br>
2022-11-17 20:57:54 -0300

    feat: excluir funcoes padroes nao utilizadas

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -62,39 +62,6 @@ class NoticeController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Notice  $notice
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Notice $notice)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Notice  $notice
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Notice $notice)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
